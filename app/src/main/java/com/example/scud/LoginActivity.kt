f5b0a7d6a3ce1a5c5a4b3b3a53d5d9a97f382045
bavtim <40119744+bavtim@users.noreply.github.com>
2022-05-14 16:33:44 +0300

    huita v2

--- app/src/main/java/com/example/scud/LoginActivity.kt
+++ app/src/main/java/com/example/scud/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.scud
 
+import android.util.Log
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
@@ -10,58 +11,61 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.annotations.SerializedName
+import okhttp3.OkHttpClient
 import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.Body
 import retrofit2.http.POST
+import retrofit2.http.Body
+import retrofit2.http.Headers
 
-object RetrofitClient {
-    private var retrofit: Retrofit? = null
+data class UserInfo (
+    @SerializedName("token") val token: String?,
+    @SerializedName("login") val login: String?,
+    @SerializedName("password") val password: String?
+)
+interface RestApi {
 
-    fun getClient(baseUrl: String): Retrofit {
-        if (retrofit == null) {
-            retrofit = Retrofit.Builder()
-                .baseUrl("http://45.90.217.136/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-        }
+    @Headers("Content-Type: application/json")
+    @POST("login")
+    fun LoginUser(@Body userData: UserInfo): Call<UserInfo>
+}
 
-        return retrofit!!
+object ServiceBuilder {
+    private val client = OkHttpClient.Builder().build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("http://45.90.217.136/") // change this IP for testing by your actual machine IP
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
+        .build()
+
+    fun<T> buildService(service: Class<T>): T{
+        return retrofit.create(service)
+    }
+}
+class RestApiService {
+    fun LoginUser(userData: UserInfo, onResult: (UserInfo?) -> Unit){
+        val retrofit = ServiceBuilder.buildService(RestApi::class.java)
+        retrofit.LoginUser(userData).enqueue(
+            object : Callback<UserInfo> {
+                override fun onFailure(call: Call<UserInfo>, t: Throwable) {
+                    //Log.i("pizdanull", t.toString())
+                    onResult(null)
+                }
+                override fun onResponse( call: Call<UserInfo>, response: Response<UserInfo>) {
+                    val addedUser = response.body()
+                    //Log.i("pizda1231", response.body().toString())
+                    onResult(addedUser)
+                }
+            }
+        )
     }
 }
 
 class LoginActivity : AppCompatActivity() {
-    public interface SCUDservice {
-        @POST("login")
-        fun login(@Body loginRequest: LoginRequest?): Call<LoginResponse?>?
-    }
-    class LoginRequest(
-        @field:SerializedName("login") var login: String,
-        @field:SerializedName("password") var password: String
-    )
-    class LoginResponse {
-        @SerializedName("data")
-        var data: Data? = null
-
-        inner class Data {
-            @SerializedName("token")
-            var token: String? = null
-        }
-
-        @SerializedName("error")
-        var error: String? = null
-
-        @SerializedName("system")
-        var system: System? = null
-
-        inner class System {
-            @SerializedName("time")
-            var time = 0.0
-        }
-    }
-
-
 
     // Объявляем об использовании следующих объектов:
     private var username: EditText? = null
@@ -72,7 +76,7 @@ class LoginActivity : AppCompatActivity() {
     private var numberOfAttempts: TextView? = null
 
     // Число для подсчета попыток залогиниться:
-    var numberOfRemainingLoginAttempts = 3
+    var numberOfRemainingLoginAttempts = 999
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -90,36 +94,47 @@ class LoginActivity : AppCompatActivity() {
 
     // Обрабатываем нажатие кнопки "Войти":
     fun Login(view: View?) {
+        val apiService = RestApiService()
+        val userInfo = UserInfo(
+            token = null,
+            login=username!!.text.toString(),
+            password = password!!.text.toString())
+        var token ="-1"
 
+        apiService.LoginUser(userInfo) {
+
+            if (it?.token != null) {
+                Log.i("pizda",token)
+                Toast.makeText(applicationContext, "Вход выполнен!", Toast.LENGTH_SHORT).show()
+
+                // Выполняем переход на другой экран:
+                val intent = Intent( this, MainActivity::class.java)
+                startActivity(intent)
+
+            } else {
+                Log.e("pizda","pizda")
+                Toast.makeText(applicationContext, "Неправильные данные!", Toast.LENGTH_SHORT).show()
+                numberOfRemainingLoginAttempts--
+
+                // Делаем видимыми текстовые поля, указывающие на количество оставшихся попыток:
+                attempts!!.visibility = View.VISIBLE
+                numberOfAttempts!!.visibility = View.VISIBLE
+                numberOfAttempts!!.text = Integer.toString(numberOfRemainingLoginAttempts)
+
+                // Когда выполнено 3 безуспешных попытки залогиниться,
+                // делаем видимым текстовое поле с надписью, что все пропало и выставляем
+                // кнопке настройку невозможности нажатия setEnabled(false):
+                if (numberOfRemainingLoginAttempts == 0) {
+                    login!!.isClickable = false
+                    loginLocked!!.visibility = View.VISIBLE
+                    loginLocked!!.setBackgroundColor(Color.RED)
+                    loginLocked!!.text = "Вход заблокирован!!!"
+                }
+            }
+        }
         // Если введенные логин и пароль будут словом "admin",
         // показываем Toast сообщение об успешном входе:
-        val loginRequest = LoginRequest(login=username!!.text.toString(), password = password!!.text.toString())
 
-        if (username!!.text.toString() == "admin" && password!!.text.toString() == "admin") {
-            Toast.makeText(applicationContext, "Вход выполнен!", Toast.LENGTH_SHORT).show()
 
-            // Выполняем переход на другой экран:
-            val intent = Intent( this, MainActivity::class.java)
-            startActivity(intent)
-        } else {
-            Toast.makeText(applicationContext, "Неправильные данные!", Toast.LENGTH_SHORT).show()
-            numberOfRemainingLoginAttempts--
-
-            // Делаем видимыми текстовые поля, указывающие на количество оставшихся попыток:
-            attempts!!.visibility = View.VISIBLE
-            numberOfAttempts!!.visibility = View.VISIBLE
-            numberOfAttempts!!.text = Integer.toString(numberOfRemainingLoginAttempts)
-
-            // Когда выполнено 3 безуспешных попытки залогиниться,
-            // делаем видимым текстовое поле с надписью, что все пропало и выставляем
-            // кнопке настройку невозможности нажатия setEnabled(false):
-            if (numberOfRemainingLoginAttempts == 0) {
-                login!!.setClickable(false)
-                loginLocked!!.visibility = View.VISIBLE
-                loginLocked!!.setBackgroundColor(Color.RED)
-                loginLocked!!.text = "Вход заблокирован!!!"
-            }
-
-        }
     }
 }
